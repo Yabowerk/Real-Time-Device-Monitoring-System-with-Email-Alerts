c78d20ab169b90d3b73286a388b5317a22f6d72e
Yabowerk <abityab34@gmail.com>
2025-07-01 13:29:52 +0300

    Add files via upload

---
+++ frontend/requesthandler.js
@@ -0,0 +1,21 @@
+
+export async function getDeviceById(id) {
+  try {
+    const response = await axios.get(`http://localhost:2000/device/${id}`);
+    console.log(response.data);
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching device:', error);
+  }
+}
+
+  export async function fetchDevices() {
+  try {
+    const response = await axios.get('http://localhost:2000/device');
+    console.log("Data received:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+fetchDevices()
